8c34e47acb8ef2700b964510891de2c3ff445309
Eugenia-d <e.drobiazko@gmail.com>
2021-11-28 18:12:11 +0200

    rendering gallery

--- src/index.js
+++ src/index.js
@@ -9,9 +9,33 @@ btn.addEventListener('click', async e => {
   e.preventDefault();
   const query = input.value;
 
-  console.log(query);
-
   const images = await getImages(query);
-
-  console.log(images);
+  onImagesReceived(images);
 });
+
+const onImagesReceived = images => {
+  console.log(images);
+  const elements = images.data.hits
+    .map(image => {
+      return `<div class="photo-card">
+  <img src="${image.previewURL}" alt="${image.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', elements);
+};
