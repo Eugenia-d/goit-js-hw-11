27b305c354674951197f31bc274514e3a191b3f5
Eugenia-d <e.drobiazko@gmail.com>
2021-11-28 18:28:34 +0200

    pagination, styles

--- src/index.js
+++ src/index.js
@@ -3,39 +3,55 @@ import { getImages } from './js/imgAPI';
 
 const input = document.querySelector('.input');
 const btn = document.querySelector('.btn');
+const btnLoadMore = document.querySelector('.load-more');
 const gallery = document.querySelector('.gallery');
+btnLoadMore.hidden = true;
+
+let query = null;
+let pageNumber = 1;
+
+const makeRequest = async () => {
+  const images = await getImages(query, pageNumber);
+  onImagesReceived(images);
+};
 
 btn.addEventListener('click', async e => {
   e.preventDefault();
-  const query = input.value;
+  query = input.value;
+  gallery.innerHTML = '';
+  btnLoadMore.hidden = true;
 
-  const images = await getImages(query);
-  onImagesReceived(images);
+  makeRequest();
 });
+
+btnLoadMore.addEventListener('click', makeRequest);
 
 const onImagesReceived = images => {
   console.log(images);
+  pageNumber += 1;
   const elements = images.data.hits
     .map(image => {
-      return `<div class="photo-card">
-  <img src="${image.previewURL}" alt="${image.tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div>`;
+      return ` <div class="photo-card">
+        <a href="${image.largeImageURL}">
+           <img src="${image.previewURL}" alt="${image.tags}" loading="lazy" width="250" height="150"/>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b> ${image.likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b> ${image.views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b> ${image.comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b> ${image.downloads}
+      </p>
+    </div></a>
+  </div>`;
     })
     .join('');
 
   gallery.insertAdjacentHTML('beforeend', elements);
+  btnLoadMore.hidden = false;
 };
--- src/js/imgAPI.js
+++ src/js/imgAPI.js
@@ -1,15 +1,18 @@
 const API_KEY = '24558958-a01582c8f081cdc4521b20935';
 const API_URL = 'https://pixabay.com/api/';
+const PAGE_SIZE = 40;
 
 import { get } from 'axios';
 
-export const getImages = query => {
+export const getImages = (query, page = 1) => {
   var url = new URL(API_URL);
   url.searchParams.append('key', API_KEY);
   url.searchParams.append('q', query);
   url.searchParams.append('image_type', 'photo');
   url.searchParams.append('orientation', 'horizontal');
   url.searchParams.append('safesearch', 'true');
+  url.searchParams.append('per_page', PAGE_SIZE);
+  url.searchParams.append('page', page);
 
   const data = get(url);
   return data;
